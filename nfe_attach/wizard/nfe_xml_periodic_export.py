8b3adf53ffb53af47264efed596da0faa3459698
bertozo <rodolfobertozo@gmail.com>
2014-12-16 16:35:47 -0200

    [FIX][UPDATE] Correção do caminho de exportação e padronização Flake8

--- nfe_attach/wizard/nfe_xml_periodic_export.py
+++ nfe_attach/wizard/nfe_xml_periodic_export.py
@@ -1,4 +1,21 @@
 # -*- encoding: utf-8 -*-
+###############################################################################
+#                                                                             #
+# Copyright (C) 2014 Rodolfo Leme Bertozo - KMEE - www.kmee.com.br            #
+#                                                                             #
+#This program is free software: you can redistribute it and/or modify         #
+#it under the terms of the GNU Affero General Public License as published by  #
+#the Free Software Foundation, either version 3 of the License, or            #
+#(at your option) any later version.                                          #
+#                                                                             #
+#This program is distributed in the hope that it will be useful,              #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+#GNU General Public License for more details.                                 #
+#                                                                             #
+#You should have received a copy of the GNU General Public License            #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+###############################################################################
 
 from openerp.osv import orm, fields
 import os
@@ -35,8 +52,8 @@ class NfeXmlPeriodicExport(orm.TransientModel):
         company_id = a._company_default_get(cr, uid)
         obj_res_company = a.browse(cr, uid, company_id)
 
-        caminho = str(obj_res_company.nfe_import_folder)
         export_dir = str(obj_res_company.nfe_export_folder)
+        caminho = export_dir
 
         #completa o caminho com homologacao ou producao
         if obj_res_company.nfe_environment == '1':
@@ -55,56 +72,72 @@ class NfeXmlPeriodicExport(orm.TransientModel):
             bkp_name = 'bkp_' + date_start[:7] + '_' + date_stop[:7] + '.zip'
 
             for diretorio in dirs_date:
-                #se houver arquivos fora do padrão (ano-mes, aaaa-mm) dentro da pasta de exportação
+                #se houver arquivos fora do padrão (ano-mes, aaaa-mm) dentro
+                #  da pasta de exportação
                 try:
-                    if (int(diretorio[:4]) >= int(date_start[:4]) and int(diretorio[5:]) >= int(date_start[5:7])) and \
-                       (int(diretorio[:4]) <= int(date_stop[:4]) and int(diretorio[5:]) <= int(date_stop[5:7])):
+                    if (int(diretorio[:4]) >= int(date_start[:4]) and
+                            int(diretorio[5:]) >= int(date_start[5:7])) and \
+                            (int(diretorio[:4]) <= int(date_stop[:4]) and
+                                int(diretorio[5:]) <= int(date_stop[5:7])):
 
                         caminho_aux = os.path.join(caminho, diretorio)
                         dirs_nfes = os.listdir(caminho_aux)
 
                         for diretorio_final in dirs_nfes:
 
-                            caminho_final = os.path.join(caminho_aux, diretorio_final) + '/'
-                            comando_cce = 'ls ' + caminho_final + '*-??-cce.xml'
-                            comando_can = 'ls ' + caminho_final + '*-??-can.xml'
-                            comando_nfe = 'ls ' + caminho_final + '*-nfe.xml| grep -E "[0-9]{44}-nfe.xml"'
-                            comando_inv = 'ls ' + caminho_final + '*-inu.xml| grep -E "[0-9]{41}-inu.xml"'
+                            caminho_final = os.path.join(caminho_aux,
+                                                         diretorio_final) + '/'
+                            comando_cce = 'ls ' + caminho_final + \
+                                          '*-??-cce.xml'
+                            comando_can = 'ls ' + caminho_final + \
+                                          '*-??-can.xml'
+                            comando_nfe = 'ls ' + caminho_final + \
+                                          '*-nfe.xml| grep -E ' \
+                                          '"[0-9]{44}-nfe.xml"'
+                            comando_inv = 'ls ' + caminho_final + \
+                                          '*-inu.xml| grep -E ' \
+                                          '"[0-9]{41}-inu.xml"'
 
                             if os.system(comando_cce) == 0:
                                 str_aux = commands.getoutput(comando_cce)
-                                caminho_arquivos = caminho_arquivos + str_aux + ' '
+                                caminho_arquivos = caminho_arquivos + \
+                                    str_aux + ' '
 
                             if os.system(comando_can) == 0:
                                 str_aux = commands.getoutput(comando_can)
-                                caminho_arquivos = caminho_arquivos + str_aux + ' '
+                                caminho_arquivos = caminho_arquivos + \
+                                    str_aux + ' '
 
                             if os.system(comando_inv) == 0:
                                 str_aux = commands.getoutput(comando_inv)
-                                caminho_arquivos = caminho_arquivos + str_aux + ' '
+                                caminho_arquivos = caminho_arquivos + \
+                                    str_aux + ' '
 
                             str_aux = commands.getoutput(comando_nfe)
                             if os.system(comando_nfe) == 0:
                                 str_aux = commands.getoutput(comando_nfe)
-                                caminho_arquivos = caminho_arquivos + str_aux + ' '
+                                caminho_arquivos = caminho_arquivos + \
+                                    str_aux + ' '
 
                         # troca \n por espaços
                         caminho_arquivos = caminho_arquivos.replace('\n', ' ')
-                        # result = os.system("zip -r " + os.path.join(export_dir, bkp_name) + ' ' + caminho_arquivos)
+                        result = os.system("zip -r " + os.path.join(export_dir,
+                                            bkp_name) + ' ' + caminho_arquivos)
 
                         data = self.read(cr, uid, ids, [], context=context)[0]
 
-                        orderFile = open(os.path.join(export_dir, bkp_name), 'r')
+                        orderFile = open(os.path.join(export_dir,
+                                            bkp_name), 'r')
                         itemFile = orderFile.read()
 
-                        self.write(cr, uid, ids, {'state': 'done', 'zip_file': base64.b64encode(itemFile),
-                                                  'name': bkp_name}, context=context)
+                        self.write(cr, uid, ids, {'state': 'done', 'zip_file':
+                                        base64.b64encode(itemFile),
+                                        'name': bkp_name}, context=context)
                 except:
                     pass
 
         if data:
-            return {
-                    'type': 'ir.actions.act_window',
+            return {'type': 'ir.actions.act_window',
                     'res_model': self._name,
                     'view_mode': 'form',
                     'view_type': 'form',
