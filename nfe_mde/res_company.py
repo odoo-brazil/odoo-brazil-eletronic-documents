26eef4d4e1ecf6569cd3a9611aefdd16acfdd5fb
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-06-22 15:43:34 -0300

    Removido import não usado

--- nfe_mde/res_company.py
+++ nfe_mde/res_company.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License    #
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 ###############################################################################
-from openerp import models, fields, api
+from openerp import models, fields
 
 
 class res_company(models.Model):
